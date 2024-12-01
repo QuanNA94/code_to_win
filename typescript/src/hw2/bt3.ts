0728953e43302516a013b85d702bbed13ff5cf45
Gideon (Quan) Nguyen <gideonnguyen94@gmail.com>
2024-12-01 23:50:45 +0700

    hw2: bt3

--- typescript/src/hw2/bt3.ts
+++ typescript/src/hw2/bt3.ts
@@ -3,15 +3,39 @@
 // Viết hàm kiểm tra xem trong một mảng các số 
 // có ít nhất một số là số nguyên tố không.
 
-function isPrime() {
+function isPrime(num: number) {
 
+    let flag = true
+
+    if (num < 2) {
+        return flag = false
+
+    } else if (num == 2) {
+        return flag = true;
+    }
+
+    else if (num % 2 == 0) {
+        return flag = false;
+    }
+
+    else {
+        // lặp từ 3 tới n-1 với bước nhảy là 2 (i+=2)
+        for (let i = 3; i < Math.sqrt(num); i += 2) {
+            if (num % i == 0) {
+                return flag = false;
+            }
+        }
+    }
+
+    return true
 }
 
 function checkAtLeastOne(primeNumbers: number[]) {
+    const filterNumber = primeNumbers.some(isPrime)
 
-    // return isPrime(primeNumbers)
+    return filterNumber
 }
 
-const primeNumbers = [2, 3, 10, 12, 13, 15, 20]
 
-console.log("kiem tra it nhat 1 so nguyen to", checkAtLeastOne(primeNumbers))
+console.log("kiem tra it nhat 1 so nguyen to", checkAtLeastOne([2, 3, 10, 12, 13, 15, 20]))
+console.log("kiem tra it nhat 1 so nguyen to", checkAtLeastOne([4, 6, 12, 15, 20])) 
